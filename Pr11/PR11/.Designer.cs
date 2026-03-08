92c96feb95c74a7daf9c72c0064fec917d262b9f
Hun1el <antonsolonikov@gmail.com>
2026-03-08 21:40:28 +0300

    Pr11. Добавлено решение практического задания.

---
+++ Pr11/PR11/.Designer.cs
@@ -0,0 +1,130 @@
+﻿using MySqlX.XDevAPI.Relational;
+
+namespace PR11
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelAverage;
+        private System.Windows.Forms.Label labelSum;
+        private System.Windows.Forms.Label labelMin;
+        private System.Windows.Forms.Label labelMax;
+        private System.Windows.Forms.Label labelCount;
+        private System.Windows.Forms.Button buttonCalculate;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelAverage = new System.Windows.Forms.Label();
+            this.labelSum = new System.Windows.Forms.Label();
+            this.labelMin = new System.Windows.Forms.Label();
+            this.labelMax = new System.Windows.Forms.Label();
+            this.labelCount = new System.Windows.Forms.Label();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(10, 10);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.Size = new System.Drawing.Size(1102, 270);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // labelAverage
+            // 
+            this.labelAverage.AutoSize = true;
+            this.labelAverage.Location = new System.Drawing.Point(12, 283);
+            this.labelAverage.Name = "labelAverage";
+            this.labelAverage.Size = new System.Drawing.Size(136, 13);
+            this.labelAverage.TabIndex = 1;
+            this.labelAverage.Text = "Средняя сумма покупки: ";
+            // 
+            // labelSum
+            // 
+            this.labelSum.AutoSize = true;
+            this.labelSum.Location = new System.Drawing.Point(12, 309);
+            this.labelSum.Name = "labelSum";
+            this.labelSum.Size = new System.Drawing.Size(47, 13);
+            this.labelSum.TabIndex = 2;
+            this.labelSum.Text = "Сумма: ";
+            // 
+            // labelMin
+            // 
+            this.labelMin.AutoSize = true;
+            this.labelMin.Location = new System.Drawing.Point(12, 335);
+            this.labelMin.Name = "labelMin";
+            this.labelMin.Size = new System.Drawing.Size(120, 13);
+            this.labelMin.TabIndex = 3;
+            this.labelMin.Text = "Минимальная сумма: ";
+            // 
+            // labelMax
+            // 
+            this.labelMax.AutoSize = true;
+            this.labelMax.Location = new System.Drawing.Point(12, 361);
+            this.labelMax.Name = "labelMax";
+            this.labelMax.Size = new System.Drawing.Size(126, 13);
+            this.labelMax.TabIndex = 4;
+            this.labelMax.Text = "Максимальная сумма: ";
+            // 
+            // labelCount
+            // 
+            this.labelCount.AutoSize = true;
+            this.labelCount.Location = new System.Drawing.Point(12, 387);
+            this.labelCount.Name = "labelCount";
+            this.labelCount.Size = new System.Drawing.Size(117, 13);
+            this.labelCount.TabIndex = 5;
+            this.labelCount.Text = "Количество записей: ";
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(15, 416);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(96, 28);
+            this.buttonCalculate.TabIndex = 6;
+            this.buttonCalculate.Text = "Рассчитать";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1124, 461);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.labelCount);
+            this.Controls.Add(this.labelMax);
+            this.Controls.Add(this.labelMin);
+            this.Controls.Add(this.labelSum);
+            this.Controls.Add(this.labelAverage);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Аптека";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Pr11/PR11/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PR11
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "server=localhost;database=PR11_Solonikov;uid=root";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM salesreceipt";
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(query, conn);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+
+                    dataGridView1.DataSource = table;
+                    dataGridView1.Columns["id"].Visible = false;
+                    table.Columns["totalprice"].ColumnName = "Сумма покупки";
+                    table.Columns["date"].ColumnName = "Дата";
+                    table.Columns["price"].ColumnName = "Цена товара";
+                    table.Columns["amount"].ColumnName = "Количество";
+                    table.Columns["name"].ColumnName = "Название";
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Ошибка загрузки: {exception.Message}");
+            }
+        }
+
+        private void Calc()
+        {
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+                    conn.Open();
+                    string avgquery = "SELECT AVG(totalprice) FROM salesreceipt";
+                    MySqlCommand avg = new MySqlCommand(avgquery, conn);
+                    double resultavg = Convert.ToDouble(avg.ExecuteScalar());
+                    labelAverage.Text = $"Средняя сумма покупки: {resultavg}";
+
+                    string sumquery = "SELECT SUM(totalprice) FROM salesreceipt";
+                    MySqlCommand sum = new MySqlCommand(sumquery, conn);
+                    double resultsum = Convert.ToDouble(sum.ExecuteScalar());
+                    labelSum.Text = $"Cумма: {resultsum}";
+
+                    string minquery = "SELECT MIN(totalprice) FROM salesreceipt";
+                    MySqlCommand min = new MySqlCommand(minquery, conn);
+                    double resultmin = Convert.ToDouble(min.ExecuteScalar());
+                    labelMin.Text = $"Минимальная сумма: {resultmin}";
+
+                    string maxquery = "SELECT MAX(totalprice) FROM salesreceipt";
+                    MySqlCommand max = new MySqlCommand(maxquery, conn);
+                    double resultmax = Convert.ToDouble(max.ExecuteScalar());
+                    labelMax.Text = $"Максимальная сумма: {resultmax}";
+
+                    string countquery = "SELECT COUNT(*) FROM salesreceipt";
+                    MySqlCommand count = new MySqlCommand(countquery, conn);
+                    int resultcount = Convert.ToInt32(count.ExecuteScalar());
+                    labelCount.Text = $"Количество записей: {resultcount}";
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show($"Ошибка расчетов: {exception.Message}");
+            }
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            Calc();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+    }
+}
