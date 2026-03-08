202513e0ab3da873ef7c660fb220dfd867b8ccb4
Hun1el <antonsolonikov@gmail.com>
2026-03-08 21:32:53 +0300

    Pr10. Добавлено решение практического задания.

---
+++ Pr10/PR10/MainForm.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using PR10;
+using System;
+using System.Windows.Forms;
+
+namespace PR10
+{
+    public partial class MainForm : Form
+    {
+        private string connectionString = "server=localhost;database=PR10_Solonikov;uid=root";
+        private MySqlConnection connection;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка подключения к базе данных: " + ex.Message);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var addForm = new AddForm(connection);
+            addForm.ShowDialog();
+        }
+
+        private void btnView_Click(object sender, EventArgs e)
+        {
+            var viewForm = new ViewForm(connection);
+            viewForm.ShowDialog();
+        }
+    }
+}
---
+++ Pr10/PR10/ViewForm.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+
+namespace PR10
+{
+    public partial class ViewForm : Form
+    {
+        private MySqlConnection connection;
+
+        public ViewForm(MySqlConnection conn)
+        {
+            InitializeComponent();
+            connection = conn;
+            LoadTickets();
+        }
+
+        private void LoadTickets()
+        {
+            string query = @"
+            SELECT 
+                TrainTicket.id AS TicketID,
+                CONCAT(Passenger.surname, ' ', Passenger.firstname) AS PassengerName,
+                Flight.trainnumber AS TrainNumber,
+                Flight.trainpath AS TrainPath,
+                TrainTicket.seatnumber AS SeatNumber,
+                TrainTicket.price AS Price
+            FROM 
+                TrainTicket
+            INNER JOIN Passenger ON TrainTicket.idpassenger = Passenger.id
+            INNER JOIN Flight ON TrainTicket.idflight = Flight.id";
+
+            try
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                    connection.Open();
+
+                var cmd = new MySqlCommand(query, connection);
+                var reader = cmd.ExecuteReader();
+
+                dataGridViewTickets.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    dataGridViewTickets.Rows.Add(
+                        reader["TicketID"],
+                        reader["PassengerName"],
+                        reader["TrainNumber"],
+                        reader["TrainPath"],
+                        reader["SeatNumber"],
+                        reader["Price"]
+                    );
+                }
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при загрузке билетов: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                    connection.Close();
+            }
+        }
+    }
+}
