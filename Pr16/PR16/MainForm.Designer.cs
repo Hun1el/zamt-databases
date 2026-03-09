9731b6386004084a92732a9121de0da821e17108
Hun1el <antonsolonikov@gmail.com>
2026-03-09 14:08:50 +0300

    Pr16. Добавлено решение практического задания.

---
+++ Pr16/PR16/MainForm.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace PR16
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox cbDelimiter;
+        private System.Windows.Forms.Label lblDelimiter;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.cbDelimiter = new System.Windows.Forms.ComboBox();
+            this.lblDelimiter = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(138, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 61);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Сгенерировать\r\nCSV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // cbDelimiter
+            // 
+            this.cbDelimiter.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbDelimiter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cbDelimiter.Items.AddRange(new object[] {
+            ",",
+            ";",
+            "$",
+            "#"});
+            this.cbDelimiter.Location = new System.Drawing.Point(166, 116);
+            this.cbDelimiter.Name = "cbDelimiter";
+            this.cbDelimiter.Size = new System.Drawing.Size(200, 33);
+            this.cbDelimiter.TabIndex = 1;
+            // 
+            // lblDelimiter
+            // 
+            this.lblDelimiter.AutoSize = true;
+            this.lblDelimiter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblDelimiter.Location = new System.Drawing.Point(12, 119);
+            this.lblDelimiter.Name = "lblDelimiter";
+            this.lblDelimiter.Size = new System.Drawing.Size(148, 25);
+            this.lblDelimiter.TabIndex = 0;
+            this.lblDelimiter.Text = "Разделитель:";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(131, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 74);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Генератор\r\nCSV файла";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::PR16.Properties.Resources.door;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(359, 300);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 49);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 361);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblDelimiter);
+            this.Controls.Add(this.cbDelimiter);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Генератор CSV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Pr16/PR16/MainForm.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PR16
+{
+    public partial class MainForm : Form
+    {
+        private string[] malefirst;
+        private string[] femalefirst;
+        private string[] malelast;
+        private string[] femalelast;
+        private string[] edu = { "Основное общее", "Среднее общее", "Среднее профессиональное", "Высшее" };
+        private string[] cities;
+        private Random random = new Random();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            malefirst = FromFile("first_names.txt");
+            malelast = FromFile("last_names.txt");
+            femalefirst = FromFile("femalefirst.txt");
+            femalelast = FromFile("femalelast.txt");
+            cities = FromFile("city.txt");
+        }
+
+        private string[] FromFile(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                return File.ReadAllLines(filename, Encoding.UTF8);
+            }
+            return new[] { "Неизвестно" };
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "CSV файлы (*.csv)|*.csv",
+                Title = "Сохранить CSV файл"
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                GenerateCSV(saveFileDialog.FileName, GetSelectedDelimiter());
+                MessageBox.Show($"Файл CSV успешно создан!\nСохранён в: {saveFileDialog.FileName}", "Готово", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private char GetSelectedDelimiter()
+        {
+            return cbDelimiter.SelectedItem.ToString()[0];
+        }
+
+        private void GenerateCSV(string filePath, char delimiter)
+        {
+            StringBuilder csvContent = new StringBuilder();
+            csvContent.AppendLine($"Фамилия{delimiter}Имя{delimiter}Дата рождения{delimiter}Образование{delimiter}Город");
+
+            for (int i = 0; i < 100000; i++)
+            {
+                bool isMale = random.Next(2) == 0;
+                string lastName;
+                string firstName;
+
+                if (isMale)
+                {
+                    lastName = GetRandom(malelast);
+                    firstName = GetRandom(malefirst);
+                }
+                else
+                {
+                    lastName = GetRandom(femalelast);
+                    firstName = GetRandom(femalefirst);
+                }
+
+                string birthDate = GenerateRandomDate();
+                string education = GetRandom(edu);
+                string city = GetRandom(cities);
+
+                csvContent.AppendLine($"{lastName}{delimiter}{firstName}{delimiter}{birthDate}{delimiter}{education}{delimiter}{city}");
+            }
+
+            File.WriteAllText(filePath, csvContent.ToString(), Encoding.UTF8);
+        }
+
+        private string GetRandom(string[] array)
+        {
+            return array[random.Next(array.Length)];
+        }
+
+        private string GenerateRandomDate()
+        {
+            DateTime start = new DateTime(1960, 1, 1);
+            DateTime end = new DateTime(2007, 12, 31);
+            return start.AddDays(random.Next((end - start).Days)).ToString("yyyy-MM-dd");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Вы действительно хотите закрыть приложение?", "Подтверждение", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                this.DialogResult = DialogResult.OK;
+                this.Close();
+            }
+        }
+    }
+}
